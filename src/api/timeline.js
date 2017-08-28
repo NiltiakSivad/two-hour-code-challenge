c09792c78493365925e7c0157eecefea14eaedd5
Kaitlin Davis <davisk56@msu.edu>
2017-08-28 16:44:42 -0500

    Add date parsing lib moment and convert raw response to Event obj

--- src/api/timeline.js
+++ src/api/timeline.js
@@ -1,8 +1,32 @@
 const timelineData = require('../json/timeline.json')
+const moment = require('moment')
+require('moment-duration-format')
+
+class TimelineEvent {
+  constructor (event) {
+    this.id = event.id
+    this.title = event.title
+    this.subtitle = event.subtitle
+    this.color = event.color
+    this.backgroundColor = event.backgroundColor
+    this.lineColor = event.lineColor
+    this.icon = event.icon
+    this.judgmentIcon = event.judgmentIcon
+    this.time = event.time
+    this.notes = event.notes
+  }
+  prettyFormattedTime () {
+    return moment.duration(this.time, 'seconds')
+      .format('HH:mm:ss', { trim: false })
+  }
+}
 
 // Fake API calls with static JSON
 export default {
   get (successCallback = () => {}) {
-    successCallback(timelineData)
+    let events = timelineData.map(event => {
+      return new TimelineEvent(event)
+    })
+    successCallback(events)
   }
 }
